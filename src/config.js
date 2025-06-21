ad73d395f657fda5f8df1a88832583bc7895f849
Chris0925-coder <christianguardia@outlook.es>
2025-06-21 17:25:16 -0500

    2312

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export const config = {
     cors: {
       server: [
         {
-          origin: "localhost:3000", //servidor que deseas que consuma o (*) en caso que sea acceso libre127.0.0.1
+          origin: "localhost", //servidor que deseas que consuma o (*) en caso que sea acceso libre127.0.0.1
           credentials: true,
         },
       ],

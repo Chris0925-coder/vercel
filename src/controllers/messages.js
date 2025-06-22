a6c139f7be55246267c9c9c65df14bc659cb6958
Chris0925-coder <christianguardia@outlook.es>
2025-06-21 23:16:26 -0500

    4444

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -9,10 +9,10 @@ const db = createClient({
 });
 
 msg.reciveMSG = async (req, res) => {
-  const query = "SELECT id,email,control FROM webdev";
+  const query = "SELECT id,email,control FROM webdev ORDER BY id DESC";
   try {
     let { rows } = await db.execute(query);
-    // console.log(rows);
+    console.log(rows);
 
     // res.json({
     //   id: rows.id,

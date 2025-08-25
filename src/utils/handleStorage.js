bc88ed6dd44972a867fffd61545aab19e64a975d
Chris0925-coder <christianguardia@outlook.es>
2025-08-24 23:28:04 -0500

    3333

--- src/utils/handleStorage.js
+++ src/utils/handleStorage.js
@@ -44,7 +44,7 @@ const videoFilter = (req, file, cb) => {
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // const pathStorage = `src/storage/img`;
-    const pathStorage = `uploads/`;
+    const pathStorage = `src/uploads/`;
     cb(null, pathStorage);
   },
   filename: function (req, file, cb) {
@@ -58,7 +58,7 @@ const storage = multer.diskStorage({
 const storageUpdate = multer.diskStorage({
   destination: function (req, file, cb) {
     // const pathStorage = `src/storage/img`;
-    const pathStorage = `uploads/`;
+    const pathStorage = `src/uploads/`;
     cb(null, pathStorage);
   },
   filename: function (req, file, cb) {

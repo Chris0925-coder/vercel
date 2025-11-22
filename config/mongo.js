b95a256b7a69230d00fcc3c9b79c4691ee7f8288
Chris0925-coder <christianguardia@outlook.es>
2025-11-22 01:10:08 -0500

    0

--- config/mongo.js
+++ config/mongo.js
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
 // const mongoAtlas = `mongodb+srv://chris30:`+encodeURIComponent(passwordDB)+`@cluster0.jkdgz4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 //mongodb+srv://chris30:<db_password>@cluster0.jkdgz4b.mongodb.net/?appName=Cluster0
 const mongoDBLocal = process.env.DB_URI;
-//"mongodb://localhost:27017/webdev";
+// const mongoDBLocal = "mongodb://localhost:27017/webdev";
 // || process.env.DB_URI
 export const dbConnect = async () => {
   try {

96ed99f9353aa5de428b63e785dddcc8b39e347c
Chris0925-coder <christianguardia@outlook.es>
2025-12-08 04:32:07 -0500

    55

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -4,33 +4,61 @@ import { TOKEN_SECRET } from "../config.js";
 export const authRequired = (req, res, next) => {
   // let { token } = req.cookies;
   const authHeader = req.headers["authorization"];
-  let token = authHeader.split(" ")[1];
 
-  if (!authHeader) token = req.cookies.token;
+  if (!authHeader) {
+    let { token } = req.cookies;
 
-  if (!token) {
-    // return res.render("login.html", {
-    //   title: "Login",
-    //   message: [],
-    // });
-    return res.status(401).json({ message: "No token, autorization denied" });
+    if (!token) {
+      // return res.render("login.html", {
+      //   title: "Login",
+      //   message: [],
+      // });
+      return res.status(401).json({ message: "No token, autorization denied" });
+    } else {
+      jwt.verify(token, TOKEN_SECRET, (err, user) => {
+        if (err) {
+          return res.status(403).json({ message: "Invalid token" });
+          // return res.render("login.html", {
+          //   title: "Login",
+          //   message: "Invalid token",
+          // });
+        } else {
+          // res.cookie("token", token);
+          // res.render("employee.html", { title: "Login", message: [] });
+          // let { user } = req.cookies;
+          req.user = user;
+
+          next();
+        }
+      });
+    }
   } else {
-    jwt.verify(token, TOKEN_SECRET, (err, user) => {
-      if (err) {
-        return res.status(403).json({ message: "Invalid token" });
-        // return res.render("login.html", {
-        //   title: "Login",
-        //   message: "Invalid token",
-        // });
-      } else {
-        // res.cookie("token", token);
-        // res.render("employee.html", { title: "Login", message: [] });
-        // let { user } = req.cookies;
-        req.user = user;
+    let token = authHeader.split(" ")[1];
 
-        next();
-      }
-    });
+    if (!token) {
+      // return res.render("login.html", {
+      //   title: "Login",
+      //   message: [],
+      // });
+      return res.status(401).json({ message: "No token, autorization denied" });
+    } else {
+      jwt.verify(token, TOKEN_SECRET, (err, user) => {
+        if (err) {
+          return res.status(403).json({ message: "Invalid token" });
+          // return res.render("login.html", {
+          //   title: "Login",
+          //   message: "Invalid token",
+          // });
+        } else {
+          // res.cookie("token", token);
+          // res.render("employee.html", { title: "Login", message: [] });
+          // let { user } = req.cookies;
+          req.user = user;
+
+          next();
+        }
+      });
+    }
   }
   console.log("Validing Token");
 };

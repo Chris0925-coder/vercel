b5c62fe44cad4bc25ae6b784a959219a13def05a
Chris0925-coder <christianguardia@outlook.es>
2026-02-25 01:05:25 -0500

    l002

--- src/controllers/form.crcv.controller.js
+++ src/controllers/form.crcv.controller.js
@@ -2,6 +2,7 @@ let msg = {};
 // process.loadEnvFile();
 
 import { createClient } from "@libsql/client";
+import { enviarCorreo } from "../utils/nodeMailer.js";
 
 const db = createClient({
   url: process.env.DB_URL,
@@ -57,6 +58,8 @@ msg.messages = async (req, res) => {
 
   try {
     await db.execute(query, params);
+
+    enviarCorreo(c.email, c.control);
 
     res.sendStatus(200).json({
       message: `User with email: ${c.email} send message ${c.control} successfully!`,
---
+++ src/utils/nodeMailer.js
@@ -0,0 +1,32 @@
+// Instalar primero: npm install nodemailer
+const nodemailer = require("nodemailer");
+
+export async function enviarCorreo(email, control) {
+  try {
+    // Configuración del transporte SMTP
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com", // Servidor SMTP
+      port: 465,
+      secure: true, // true para 465, false para otros puertos
+      auth: {
+        user: "christianguardia@outlook.es",
+        pass: "tu_contraseña_o_token_app", // Usa contraseñas de aplicación
+      },
+    });
+
+    // Datos del correo
+    let info = await transporter.sendMail({
+      from: '"CRCV Panamá" <christianguardia@outlook.es>',
+      to: `${email}`,
+      subject: "Prueba de envío",
+      text: `${control}`,
+      html: `<div class="foot-rights">
+				<p>Copyright © <span class="date"></span> <a href="https://www.instagram.com/crcvpanama/" target="_blank"><strong> Centro de Rehabilitación de la Columna Vertebral</strong></a> </p>
+			</div>`,
+    });
+
+    console.log("Correo enviado:", info.messageId);
+  } catch (error) {
+    console.error("Error al enviar correo:", error);
+  }
+}

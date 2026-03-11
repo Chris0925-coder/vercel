5b89bb3bd0848e2529b64c1426feedbe080aa111
Chris0925-coder <christianguardia@outlook.es>
2026-03-11 18:48:36 -0500

    0

--- src/controllers/views.count.controller.js
+++ src/controllers/views.count.controller.js
@@ -8,13 +8,14 @@ const db = createClient({
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
-controller.loginPage = async (req, res) => {
+controller.loginPage = async (req, res, next) => {
   const { token } = req.cookies;
   try {
     if (!token)
       return res.render("login.html", { title: "LOGIN", tab: [], message: [] });
 
-    res.redirect("/home");
+    // res.redirect("/home");
+    next();
   } catch (error) {
     res.status(500).json({ message: error.message });
   }

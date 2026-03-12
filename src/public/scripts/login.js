11817b5deefbea224bb9d9d679782be787b6ba36
Chris0925-coder <christianguardia@outlook.es>
2026-03-11 19:11:02 -0500

    0

--- src/public/scripts/login.js
+++ src/public/scripts/login.js
@@ -84,6 +84,8 @@ async function login() {
     })
       .then((response) => response.json())
       .catch((error) => {
+        removeCookie("token");
+        window.location.replace("/");
         console.error("Error:", error);
         message.style.color = "#990000";
         message.innerText = error;
@@ -95,8 +97,6 @@ async function login() {
       setCookie("token", result, 365);
       window.location.replace("/home");
     } else {
-      removeCookie("token");
-      window.location.replace("/");
       message.style.color = "#990000";
       message.innerText = result.error;
     }

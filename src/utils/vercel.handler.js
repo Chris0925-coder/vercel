89931da0c0c4ee76ab50da7fd213611aed0f6e1d
Chris0925-coder <christianguardia@outlook.es>
2026-03-26 12:36:48 -0500

    0

--- src/utils/vercel.handler.js
+++ src/utils/vercel.handler.js
@@ -24,6 +24,32 @@ export async function PUT(req, res, next) {
   } else {
     try {
       const fileContent = files.buffer;
+      const blob = await put(files.originalname, fileContent, {
+        access: "public", // or "private" depending on your needs
+        token: process.env.BLOB_READ_WRITE_TOKEN, // Ensure this token is set in your environment variables
+        allowOverwrite: true,
+      });
+      // console.log("File uploaded successfully:", blob.url);
+      // return Response.json(blob);
+      // Returns the public URL of the uploaded file
+      // return blob.url;
+      next();
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      throw error;
+    }
+  }
+}
+
+export async function PUT_SALES(req, res, next) {
+  const files = await req.file;
+  // console.log(!files);
+  if (!files) {
+    // files = existData.rows[0].images;
+    next();
+  } else {
+    try {
+      const fileContent = files.buffer;
 
       const blob = await put(`sales/${files.originalname}`, fileContent, {
         access: "public", // or "private" depending on your needs

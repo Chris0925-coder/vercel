05a78d2ad6b5f9bd708fe559c2e61816ecc6ae19
Chris0925-coder <christianguardia@outlook.es>
2026-04-07 02:06:51 -0500

    0

--- src/public/scripts/fondation.js
+++ src/public/scripts/fondation.js
@@ -1,5 +1,11 @@
 const date = new Date();
-const footerDate = document.querySelector('.date');
+const footerDate = document.querySelector(".date");
+
+const menu = document.querySelector(".btn-menu");
+const nav = document.querySelector(".nav-bar");
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
 
 footerDate.append(date.getFullYear());
-console.log('Hola')

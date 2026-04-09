18e0716bc9c828a3450346b123a645fbb108fbe2
Chris0925-coder <christianguardia@outlook.es>
2026-04-09 11:16:29 -0500

    0

--- src/public/scripts/fundation.js
+++ src/public/scripts/fundation.js
@@ -4,8 +4,16 @@ const footerDate = document.querySelector(".date");
 const menu = document.querySelector(".btn-menu");
 const nav = document.querySelector(".nav-bar");
 
-menu.addEventListener("click", () => {
+menu.addEventListener("click", (e) => {
   nav.classList.toggle("show");
+
+  navListen();
 });
+
+function navListen() {
+  nav.addEventListener("click", (e) => {
+    if (e.target.tagName == "A") nav.classList.remove("show");
+  });
+}
 
 footerDate.append(date.getFullYear());

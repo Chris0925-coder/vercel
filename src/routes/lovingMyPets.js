754a05672bdca4c98e6855a9557eb2439d5e5adb
Chris0925-coder <christianguardia@outlook.es>
2026-05-08 09:54:02 -0500

    0

--- src/routes/lovingMyPets.js
+++ src/routes/lovingMyPets.js
@@ -26,7 +26,7 @@ router.post("/login", validateSchema(loginSchema), login);
 router.post(
   "/",
   authRequired,
-  uploadMiddleware.array("filename[]", 3),
+  uploadMiddleware.array("filename", 3),
   PUT_LMP,
   storageController.article,
 );

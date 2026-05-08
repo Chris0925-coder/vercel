bd184f8e5cb145b160cfd5fc362a1f961402fcf0
Chris0925-coder <christianguardia@outlook.es>
2026-05-08 09:46:26 -0500

    0

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -13,7 +13,7 @@ export class ServerError extends Error {
   }
 }
 
-const uploadImg = uploadMiddleware.array("filename[]", 3);
+const uploadImg = uploadMiddleware.array("filename[]");
 
 export const multerErr = (req, res, next) => {
   uploadImg(req, res, function (err) {

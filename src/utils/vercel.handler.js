202712acbce063b1fb769ccad7742275f1cde6b6
Chris0925-coder <christianguardia@outlook.es>
2026-05-08 07:17:34 -0500

    0

--- src/utils/vercel.handler.js
+++ src/utils/vercel.handler.js
@@ -15,7 +15,7 @@ import { put } from "@vercel/blob";
 //   next();
 // }
 
-export async function PUT(req, res, next) {
+export async function PUT_LMP(req, res, next) {
   const { files, body } = await req;
   // console.log(!files);
   if (!files) {
@@ -25,11 +25,15 @@ export async function PUT(req, res, next) {
     try {
       for (let i = 0; i < files.length; i++) {
         const fileContent = files[i].buffer;
-        const blob = await put(files[i].originalname, fileContent, {
-          access: "public", // or "private" depending on your needs
-          token: process.env.BLOB_READ_WRITE_TOKEN, // Ensure this token is set in your environment variables
-          allowOverwrite: true,
-        });
+        const blob = await put(
+          `lovingmypets/${files[i].originalname}`,
+          fileContent,
+          {
+            access: "public", // or "private" depending on your needs
+            token: process.env.BLOB_READ_WRITE_TOKEN, // Ensure this token is set in your environment variables
+            allowOverwrite: true,
+          },
+        );
       }
       // console.log("File uploaded successfully:", blob.url);
       // return Response.json(blob);
@@ -43,7 +47,7 @@ export async function PUT(req, res, next) {
   }
 }
 
-export async function PUT_LMP(req, res, next) {
+export async function PUT(req, res, next) {
   const files = await req.file;
   // console.log(!files);
   if (!files) {
@@ -53,15 +57,11 @@ export async function PUT_LMP(req, res, next) {
     try {
       const fileContent = files.buffer;
 
-      const blob = await put(
-        `lovingmypets/${files.originalname}`,
-        fileContent,
-        {
-          access: "public", // or "private" depending on your needs
-          token: process.env.BLOB_READ_WRITE_TOKEN, // Ensure this token is set in your environment variables
-          allowOverwrite: true,
-        },
-      );
+      const blob = await put(files.originalname, fileContent, {
+        access: "public", // or "private" depending on your needs
+        token: process.env.BLOB_READ_WRITE_TOKEN, // Ensure this token is set in your environment variables
+        allowOverwrite: true,
+      });
       // console.log("File uploaded successfully:", blob.url);
       // return Response.json(blob);
       // Returns the public URL of the uploaded file

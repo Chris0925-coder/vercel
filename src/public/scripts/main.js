e8a61360c23ff18c4615aa81668f23d0e80bf6df
Chris0925-coder <christianguardia@outlook.es>
2026-06-13 22:24:38 -0500

    0

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -105,7 +105,8 @@ function dif() {
   let c = document.getElementsByClassName("count-views");
 
   let m = Object.values(c).map((item, index) => {
-    if (!detectCookie(`count${index}`)) setCookie(`count${index}`, "");
+    console.log(!detectCookie(`count0`));
+    if (!detectCookie(`count0`)) setCookie(`count${index}`, "");
     let e = item.textContent;
 
     let s = { [index]: c[index].textContent };

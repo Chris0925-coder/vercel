5998068c97245779abff4d5318807bba72d1b0f9
Chris0925-coder <christianguardia@outlook.es>
2026-06-29 14:27:32 -0500

    0

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -4,36 +4,40 @@ import { TOKEN_SECRET } from "../config.js";
 export const authRequired = (req, res, next) => {
   const authHeader = req.headers["authorization"];
 
-  if (!authHeader) {
-    let { token } = req.cookies;
+  // if (!authHeader) {
+  let { token } = req.cookies;
 
-    if (!token)
-      return res.status(401).json({ message: "No token, autorization denied" });
+  if (!token)
+    return res.status(401).json({ message: "No token, autorization denied" });
 
+  if (token) {
     jwt.verify(token, TOKEN_SECRET, (err, user) => {
       if (err) {
         return res.status(403).json({ message: "Invalid token" });
       } else {
         req.user = user;
 
-        next();
-      }
-    });
-  } else {
-    let token = authHeader.split(" ")[1];
-
-    if (!token)
-      return res.status(401).json({ message: "No token, autorization denied" });
-
-    jwt.verify(token, TOKEN_SECRET, (err, user) => {
-      if (err) {
-        return res.status(403).json({ message: "Invalid token" });
-      } else {
-        req.user = user;
-        next();
+        // next();
       }
     });
   }
+  // } else {
+  let tokenAuth = authHeader.split(" ")[1];
+
+  if (!tokenAuth)
+    return res.status(401).json({ message: "No token, autorization denied" });
+
+  if (tokenAuth) {
+    jwt.verify(token, TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: "Invalid token" });
+      } else {
+        req.user = user;
+        // next();
+      }
+    });
+  }
+  // }
   console.log("Validing Token");
-  // next();
+  next();
 };

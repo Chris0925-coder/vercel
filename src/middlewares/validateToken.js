81efbf27ee8caf5c5402aa4c2627feccc0baac90
Chris0925-coder <christianguardia@outlook.es>
2026-06-29 14:55:05 -0500

    0

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -4,44 +4,38 @@ import { TOKEN_SECRET } from "../config.js";
 export const authRequired = (req, res, next) => {
   let authHeader = req.headers["authorization"];
   let { token } = req.cookies;
+  if (!token)
+    return res.status(401).json({
+      message: "No token, autorization denied (*)",
+      token: req.cookies,
+    });
 
   if (!authHeader) {
-    if (!token)
-      return res.status(401).json({
-        message: "No token, autorization denied (*)",
-        token: req.cookies,
-      });
+    jwt.verify(token, TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: "Invalid token" });
+      } else {
+        req.user = user;
 
-    if (token) {
-      jwt.verify(token, TOKEN_SECRET, (err, user) => {
-        if (err) {
-          return res.status(403).json({ message: "Invalid token" });
-        } else {
-          req.user = user;
+        // next();
+      }
+    });
+  }
+  // } else {
+  let tokenAuth = authHeader.split(" ")[1];
 
-          // next();
-        }
-      });
-    }
+  if (!tokenAuth) {
+    return res.status(401).json({
+      message: "No token, autorization denied",
+      tokenAuth: tokenAuth,
+    });
   } else {
-    let tokenAuth = authHeader.split(" ")[1];
+    jwt.verify(token, TOKEN_SECRET, (err, user) => {
+      if (err) return res.status(403).json({ message: "Invalid token" });
 
-    if (!tokenAuth)
-      return res.status(401).json({
-        message: "No token, autorization denied",
-        tokenAuth: tokenAuth,
-      });
-
-    if (tokenAuth) {
-      jwt.verify(token, TOKEN_SECRET, (err, user) => {
-        if (err) {
-          return res.status(403).json({ message: "Invalid token" });
-        } else {
-          req.user = user;
-          // next();
-        }
-      });
-    }
+      req.user = user;
+      // next();
+    });
   }
   console.log("Validing Token");
   next();
